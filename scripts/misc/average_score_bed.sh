8a4c9005330f02e4570a49dcc248a0fd83985744
sepour <31295509+spour@users.noreply.github.com>
2023-02-03 15:36:17 -0500

    Update average_score_bed.sh

--- scripts/misc/average_score_bed.sh
+++ scripts/misc/average_score_bed.sh
@@ -1,2 +1,2 @@
 #Purpose: get average score of the baseline scores for all the CPA sites in a bed file
-awk '!seen[$1$2$3$4$5$6]++' $bed | awk '{ total += $11 } END { print total/NR }'
+awk '!seen[$1$2$3$4$5$6]++' $bed | awk '{ total += $5 } END { print total/NR }'
